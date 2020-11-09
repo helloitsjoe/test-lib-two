2bd3302da6692c1dd2238e0dae819dffcd018a10
helloitsjoe <jboyle617@gmail.com>
2020-11-09 15:03:00 -0500

    test

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const config = makeWebpackConfig();
 
 config.output = {
   library: 'test-two',
-  libraryTarget: 'amd',
+  libraryTarget: 'umd',
 };
 config.mode = 'development';
 config.externals = { react: 'react', 'react-dom': 'react-dom' };
